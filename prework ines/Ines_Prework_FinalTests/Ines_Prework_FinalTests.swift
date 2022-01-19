e7825c79252c7b1c73d8607885cdd5ecaa5c03d9
ibelkhodja <97632864+ibelkhodja@users.noreply.github.com>
2022-01-19 10:48:49 -0500

    added files to repository

---
+++ prework ines/Ines_Prework_FinalTests/Ines_Prework_FinalTests.swift
@@ -0,0 +1,34 @@
+//
+//  Ines_Prework_FinalTests.swift
+//  Ines_Prework_FinalTests
+//
+//  Created by Ines Belkhodja on 1/18/22.
+//  Copyright © 2022 Ines Belkhodja. All rights reserved.
+//
+
+import XCTest
+@testable import Ines_Prework_Final
+
+class Ines_Prework_FinalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
